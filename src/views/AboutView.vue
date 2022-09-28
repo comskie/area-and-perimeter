74fe3a95a261f1f1c48e028890fd8dbd877e3cdf
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-28 20:50:44 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,9 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <a href="https://www.flaticon.com/free-icons/shapes" title="shapes icons">
+      Shapes icons created by Pixel perfect - Flaticon
+    </a>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
