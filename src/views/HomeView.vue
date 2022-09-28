7c1622edcb5a26c2c2a3895dcc3be55dfc78a8ae
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-28 20:51:15 +0800

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,69 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
-</script>
+import ShapeCard from "../components/ShapeCard.vue";
+import { useDark, useToggle } from "@vueuse/core";
+import { ref } from "vue";
+import SquareCalculator from "../components/SquareCalculator.vue";
+import RectangleCalculator from "../components/RectangleCalculator.vue";
 
+const isDark = useDark();
+const toggleDark = useToggle(isDark);
+
+const shapes = ["square", "rectangle", "triangle"];
+
+const currentShape = ref(shapes[0]);
+</script>
 <template>
-  <main>
-    <TheWelcome />
+  <header class="absolute inset-x-0 top-0">
+    <div class="container flex justify-end p-4">
+      <button @click="toggleDark()" class="p-2 dark:text-white">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="hidden h-6 w-6 dark:block"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+          />
+        </svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="block h-6 w-6 dark:hidden"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+          />
+        </svg>
+      </button>
+    </div>
+  </header>
+  <main
+    class="grid min-h-screen justify-center transition-all ease-in dark:bg-neutral-800 dark:text-white"
+  >
+    <div class="flex h-[50vh] items-end">
+      <div class="flex flex-row gap-4">
+        <ShapeCard shape="rectangle" @click="currentShape = 'rectangle'" />
+        <ShapeCard shape="square" @click="currentShape = 'square'" />
+        <ShapeCard shape="triangle" @click="currentShape = 'triangle'" />
+      </div>
+    </div>
+    <div class="h-[50vh]">
+      <div v-if="currentShape === 'square'" class="mt-12">
+        <SquareCalculator />
+      </div>
+      <div v-if="currentShape === 'rectangle'" class="mt-12">
+        <RectangleCalculator />
+      </div>
+    </div>
   </main>
 </template>
