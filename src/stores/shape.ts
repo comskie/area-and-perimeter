1ddd344bdfd269812413f46f78edf7b18a15d043
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-29 12:44:25 +0800

    Add shape.ts for storing current shape and store it in session storage

---
+++ src/stores/shape.ts
@@ -0,0 +1,19 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import { useLocalStorage, useSessionStorage } from "@vueuse/core";
+
+type ShapeString = "square" | "rectangle" | "triangle";
+
+export const useShapeStore = defineStore("shape", () => {
+  const shape = useSessionStorage("shape", <ShapeString>"square");
+
+  function setShape(newShape: ShapeString) {
+    shape.value = newShape;
+  }
+
+  function isShape(shapeToCheck: ShapeString) {
+    return shape.value === shapeToCheck;
+  }
+
+  return { setShape, isShape };
+});
