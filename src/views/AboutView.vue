9783d3d804986232f10b5f707aafbe1b91a1ecd4
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-29 15:15:42 +0800

    Delete AboutView.vue

--- src/views/AboutView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-
-    <a href="https://www.flaticon.com/free-icons/shapes" title="shapes icons">
-      Shapes icons created by Pixel perfect - Flaticon
-    </a>
-  </div>
-</template>
