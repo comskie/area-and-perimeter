35a2164415b619ac2d7a6f68c61aae960bfc7604
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-29 16:57:38 +0800

    Update ShapeCard.vue

--- src/components/ShapeCard.vue
+++ src/components/ShapeCard.vue
@@ -20,7 +20,7 @@ const imagePath = new URL(`../assets/icons/${props.shape}.png`, import.meta.url)
   <button
     ref="target"
     :class="[
-      'flex cursor-pointer select-none flex-col items-center justify-center space-y-4 rounded-md border-indigo-600 bg-indigo-500 p-4  shadow-indigo-200 transition-all duration-200  ease-out dark:bg-indigo-600 dark:shadow-indigo-600/30',
+      'flex cursor-pointer select-none flex-col items-center justify-center space-y-4 rounded-md border-indigo-600 bg-indigo-500 p-4  shadow-indigo-500/50 transition-all duration-200  ease-out dark:bg-indigo-600 dark:shadow-indigo-600/30',
       isActive
         ? '-translate-y-2 scale-105 font-bold shadow-xl'
         : 'shadow hover:-translate-y-1 hover:shadow-lg',
@@ -29,7 +29,7 @@ const imagePath = new URL(`../assets/icons/${props.shape}.png`, import.meta.url)
     <img class="mt-2" :src="imagePath" :alt="shape" height="80" width="80" />
     <div>
       <span
-        class="font text-lg capitalize text-stone-100 dark:text-white sm:text-2xl"
+        class="text-lg capitalize font text-stone-100 dark:text-white sm:text-2xl"
         >{{ shape }}</span
       >
     </div>
